6af8defca04e45d44b0109f829b12ad140a5719d
Liempo <gonzalesalec@gmail.com>
2023-11-02 07:39:48 +0800

    Add `@vercel/kv`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { purgeCss } from 'vite-plugin-tailwind-purgecss'
+import dotEnvExpand from 'dotenv-expand'
 
-export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	if (mode === 'development') {
+		const env = loadEnv(mode, process.cwd(), '')
+		dotEnvExpand.expand({ parsed: env })
+	}
+	return {
+		plugins: [sveltekit(), purgeCss()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
 	}
 })
