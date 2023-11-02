14a4d77fab1e90ebf4acf36b81f8505ad5838a82
Liempo <gonzalesalec@gmail.com>
2023-11-02 15:07:53 +0800

    Use localStorage for userStore

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,14 @@
 import { writable } from 'svelte/store'
+import { localStorageStore } from '@skeletonlabs/skeleton'
 import type { User } from '@/types/app'
 
 // Current user profile (name, audio, video)
-export const userStore = writable<User>()
+// export const userStore = writable<User>()
+export const userStore = localStorageStore<User>('user', {
+	name: '',
+	audio: true,
+	video: true
+})
 
 // List of members in the room including the current user
 export const membersStore = writable<User[]>([])
