af59437e6a206d948615723dc96fd00edf4d0de7
Alec <gonzalesalec@gmail.com>
2023-11-25 19:45:43 +0800

    Remove @vercel/kv

--- src/routes/api/user/[id]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { getUserData } from '@/lib/kv'
-import type { RequestHandler } from './$types'
-
-export const GET: RequestHandler = async ({ params: { id } }) => {
-	const uid = parseInt(id)
-	const data = await getUserData(uid)
-	return new Response(JSON.stringify(data))
-}
